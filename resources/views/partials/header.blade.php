9c53260fa129fcca2e10a0811a3d9dd40f34a315
longstl <jonnykhanh1102@gmail.com>
2018-07-30 20:34:25 +0700

    edit nav bar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark pb_navbar pb_scrolled-light bg-dark" id="pb-navbar">
+<nav class="navbar navbar-expand-lg navbar-dark pb_navbar pb_scrolled-light bg-dark" id="pb-navbar" style="height: 50px">
     <div class="container">
         <a class="navbar-brand d-xl-none d-lg-none d-md-block d-sm-block" href="/">
             <img src="images/logo/logo.png" alt="Instant Logo" class="light" style="width: 80px; height: 80px;">
@@ -8,17 +8,17 @@
         </button>
         <div class="collapse navbar-collapse justify-content-md-center" id="probootstrap-navbar">
             <ul class="navbar-nav">
-                <li class="nav-item" style="margin-top: 30px; margin-bottom: 30px;"><a class="nav-link text-uppercase pb_letter-spacing-2" href="#section-home">Home</a></li>
-                <li class="nav-item" style="margin-top: 30px; margin-bottom: 30px;"><a class="nav-link text-uppercase pb_letter-spacing-2" href="#section-about">About</a></li>
-                <li class="nav-item" style="margin-top: 30px; margin-bottom: 30px;"><a class="nav-link text-uppercase pb_letter-spacing-2" href="#section-menu">Menu</a></li>
+                <li class="nav-item" style="margin-top: 30px; margin-bottom: 30px;"><a class="nav-link text-uppercase pb_letter-spacing-2" href="#">Home</a></li>
+                <li class="nav-item" style="margin-top: 30px; margin-bottom: 30px;"><a class="nav-link text-uppercase pb_letter-spacing-2" href="#">About</a></li>
+                <li class="nav-item" style="margin-top: 30px; margin-bottom: 30px;"><a class="nav-link text-uppercase pb_letter-spacing-2" href="#">Adopt a Pet</a></li>
                 <li class="nav-item logo-center d-xl-block d-lg-block d-md-none d-sm-none d-none">
                     <a class="nav-link text-uppercase pb_letter-spacing-2" href="/">
                         <img src="images/logo/logo.png" alt="Instant Logo" class="light" style="width: 80px; height: 80px;">
                     </a>
                 </li>
-                <li class="nav-item" style="margin-top: 30px; margin-bottom: 30px;"><a class="nav-link text-uppercase pb_letter-spacing-2" href="#section-gallery">Gallery</a></li>
-                <li class="nav-item" style="margin-top: 30px; margin-bottom: 30px;"><a class="nav-link text-uppercase pb_letter-spacing-2" href="#section-events">Events</a></li>
-                <li class="nav-item" style="margin-top: 30px; margin-bottom: 30px;"><a class="nav-link text-uppercase pb_letter-spacing-2" href="#section-contact">Contact</a></li>
+                <li class="nav-item" style="margin-top: 30px; margin-bottom: 30px;"><a class="nav-link text-uppercase pb_letter-spacing-2" href="#">Shop Accessories</a></li>
+                <li class="nav-item" style="margin-top: 30px; margin-bottom: 30px;"><a class="nav-link text-uppercase pb_letter-spacing-2" href="#">Pet Spa</a></li>
+                <li class="nav-item" style="margin-top: 30px; margin-bottom: 30px;"><a class="nav-link text-uppercase pb_letter-spacing-2" href="#">Contact</a></li>
             </ul>
         </div>
     </div>
