44a7c52ba2cf95025e0c5a1dfd4de78e24782107
longstl <jonnykhanh1102@gmail.com>
2018-07-30 15:10:28 +0700

    add template dashboard

--- resources/views/partials/slideshow.blade.php
+++ resources/views/partials/slideshow.blade.php
@@ -1,11 +1,15 @@
 <div id="captioned-gallery">
     <figure class="slider">
         <figure>
-            <img src="http://s1.1zoom.me/b4757/639/Cats_Dogs_Juice_Two_Poodle_Glasses_Funny_526822_1280x720.jpg" alt="">
+            <img src="https://anhdephd.com/wp-content/uploads/2018/01/hinh-anh-chu-cho-dep-va-ca-tinh.jpg" alt="">
             <figcaption>PET HOUSE</figcaption>
         </figure>
         <figure>
-            <img src="https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/Vm8N8ry5eikbknln4/dog-and-cat-on-a-white-background_siomhko__F0000.png" alt>
+            <img src="http://s1.1zoom.me/b4757/639/Cats_Dogs_Juice_Two_Poodle_Glasses_Funny_526822_1280x720.jpg" alt>
+            <figcaption>PET HOUSE</figcaption>
+        </figure>
+        <figure>
+            <img src="http://www.petcity.vn/media/news/2607_hinh-anh-meo-con-de-thuong-dang-yeu-dep-3.jpg" alt="">
             <figcaption>PET HOUSE</figcaption>
         </figure>
         <figure>
@@ -13,11 +17,7 @@
             <figcaption>PET HOUSE</figcaption>
         </figure>
         <figure>
-            <img src="http://s1.1zoom.me/b5050/283/Dogs_Cats_Grass_Chihuahua_Border_Collie_514094_1280x720.jpg" alt="">
-            <figcaption>PET HOUSE</figcaption>
-        </figure>
-        <figure>
-            <img src="https://s1-ssl.dmcdn.net/NdFtL/x720-R2F.jpgs" alt="">
+            <img src="https://upload.chjoy.com/uploads/1529013118721.jpg" alt="">
             <figcaption>PET HOUSE</figcaption>
         </figure>
     </figure>
